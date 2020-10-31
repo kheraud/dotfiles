f09fb4e5935e8ebc298958731ea9bf07a8053a0c
kheraud <kheraud@fastmail.com>
2020-10-31 23:09:56 +0100

    fix(urxvt): fix urxvt launch with tmux workspace

--- roles/urxvt/files/launch-in-workspace.sh
+++ roles/urxvt/files/launch-in-workspace.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 xrdb -merge ~/.Xresources
-urxvt -sl 0 -e sh -c 'tmux new-session -A -s "workspace-$(xdotool get_desktop)"'
+CONSOLE_TITLE="workspace-$(xdotool get_desktop)"
+urxvt -sl 0 -title "${CONSOLE_TITLE}" -e tmux new-session -A -s "${CONSOLE_TITLE}"
