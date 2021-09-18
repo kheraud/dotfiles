2c42c4337201e271784fe809b12889201cca35da
kheraud <1894658+kheraud@users.noreply.github.com>
2021-09-18 15:01:31 +0200

    fix: force load of Xresource on terminal launches

--- roles/desktop/files/launch-in-workspace.sh
+++ roles/desktop/files/launch-in-workspace.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-xrdb -merge ~/.Xresources
+xrdb -load ~/.Xresources
 CONSOLE_TITLE="workspace-$(xdotool get_desktop)"
 urxvt -sl 0 -title "${CONSOLE_TITLE}" -e tmux new-session -A -s "${CONSOLE_TITLE}"
