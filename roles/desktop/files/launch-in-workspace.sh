85bbb47f3a4952c37670c52e639f1c44751ede70
kheraud <1894658+kheraud@users.noreply.github.com>
2021-10-21 21:05:50 +0200

    feat(urxvt): add test in shell shortcut to avoid launching two times

--- roles/desktop/files/launch-in-workspace.sh
+++ roles/desktop/files/launch-in-workspace.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 xrdb -load ~/.Xresources
 CONSOLE_TITLE="workspace-$(xdotool get_desktop)"
+
+wmctrl -a "${CONSOLE_TITLE}" ||
 urxvt -sl 0 -title "${CONSOLE_TITLE}" -e tmux new-session -A -s "${CONSOLE_TITLE}"
