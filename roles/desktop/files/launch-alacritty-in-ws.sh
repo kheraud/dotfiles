ee383e864ffc9fdc5db991b4b33773b249c78813
Karim Heraud <1894658+kheraud@users.noreply.github.com>
2024-05-03 23:30:09 +0200

    feat: improve shell rendering (#18)
    * feat: replace urxvt with alacritty
    
    * feat: replace keyboard shortcut for alacritty
    
    * feat: change tmux status bar

--- roles/desktop/files/launch-alacritty-in-ws.sh
+++ roles/desktop/files/launch-alacritty-in-ws.sh
@@ -3,4 +3,4 @@ xrdb -load ~/.Xresources
 CONSOLE_TITLE="workspace-$(xdotool get_desktop)"
 
 wmctrl -a "${CONSOLE_TITLE}" ||
-urxvt -sl 0 -title "${CONSOLE_TITLE}" -e tmux new-session -A -s "${CONSOLE_TITLE}"
+alacritty --title "${CONSOLE_TITLE}" --command tmux new-session -A -s "${CONSOLE_TITLE}"
