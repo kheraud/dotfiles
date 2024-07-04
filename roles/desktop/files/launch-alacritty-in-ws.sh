86a2ac585238559f261fefdef23dbd0141073dc4
kheraud <1894658+kheraud@users.noreply.github.com>
2024-07-04 12:17:22 +0200

    feat: reduce name of workspace in tmux sessions

--- roles/desktop/files/launch-alacritty-in-ws.sh
+++ roles/desktop/files/launch-alacritty-in-ws.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 xrdb -load ~/.Xresources
-CONSOLE_TITLE="workspace-$(xdotool get_desktop)"
+CONSOLE_TITLE="wrk-$(xdotool get_desktop)"
 
 wmctrl -a "${CONSOLE_TITLE}" ||
 alacritty --title "${CONSOLE_TITLE}" --command tmux new-session -A -s "${CONSOLE_TITLE}"
